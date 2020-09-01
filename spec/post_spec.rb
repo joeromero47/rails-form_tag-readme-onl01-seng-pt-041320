9d0bddce7d9c30836f8be0122119902c4cacd5b1
joeromero47 <joeromero47@gmail.com>
2020-09-01 12:01:05 -0700

    Done.

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
- 
+require 'pry'
+
 describe 'new post' do
   it 'ensures that the form route works with the /new action' do
+    binding.pry
     visit new_post_path
     expect(page.status_code).to eq(200)
   end
